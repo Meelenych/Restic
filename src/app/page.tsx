ccdcb128ddca9ef7d332b00b3f6e1db0742b7f38
Meelenych <meelenych@gmail.com>
2024-06-17 22:49:37 -0600

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<main className='flex justify-items-center items-center'>
 			<h1 className='text-blue-600 text-lg font-bold'>New Restic</h1>
-			<p>Welcome to our restaurant!</p>
+			<p className='text-sm text-cyan-900'>Welcome to our restaurant!</p>
 		</main>
 	);
 }
