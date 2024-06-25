5dd344da1f6c182371cf00d33e6f1991a5fc3015
Meelenych <meelenych@gmail.com>
2024-06-24 22:21:09 -0600

    rounded letters

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,23 @@ import Navigation from './Navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import planetXprs from '../assets/png/Planet_Exp.png';
+import styles from '../styles/Footer.module.css';
+
+const bendText = text => {
+	const length = text.length;
+	return text.split('').map((char, index) => {
+		const angle = (index / length) * 360;
+		return (
+			<span
+				key={index}
+				style={{
+					transform: `rotate(${angle}deg) translate(90px) rotate(-${angle}deg)`,
+				}}>
+				{char}
+			</span>
+		);
+	});
+};
 
 const Footer = () => {
 	return (
@@ -17,15 +34,64 @@ const Footer = () => {
 						className='hover:animate-flip-x mr-5'
 					/>
 					<div className='flex w-full'>
-						<Link
-							href='/delivery'
-							className='flex items-center justify-center h-40 w-40 rounded-full border-emerald-500 bg-emerald-300 text-yellow-100 text-xl hover:-translate-y-1 hover:drop-shadow-2xl active:drop-shadow-lg active:translate-y-0 transition ease-out duration-300 mr-4'>
-							Order delivery
+						<Link href='/delivery'>
+							<div className='w-30 h-30 p-3 relative border-4 border-black bg-amber-50 rounded-full hover:-translate-y-1 hover:drop-shadow-2xl active:drop-shadow-lg active:translate-y-0 transition ease-out duration-300 mr-4'>
+								<svg
+									width='100'
+									height='100'
+									viewBox='0 0 100 100'
+									xmlns='http://www.w3.org/2000/svg'>
+									<defs>
+										<path
+											id='circlePath'
+											d='
+            M 50, 50
+            m -40, 0
+            a 40,40 0 1,1 80,0
+            a 40,40 0 1,1 -80,0
+          '
+										/>
+									</defs>
+									<text>
+										<textPath
+											href='#circlePath'
+											className='font-extrabold text-xl'>
+											ORDER DELIVERY
+										</textPath>
+									</text>
+								</svg>
+								<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 border-black bg-red-600 w-20 h-20 rounded-full'></div>
+							</div>
 						</Link>
-						<Link
-							href='/book'
-							className='flex items-center justify-center h-40 w-40 rounded-full border-emerald-500 bg-emerald-300 text-yellow-100 text-xl hover:-translate-y-1 hover:drop-shadow-2xl active:drop-shadow-lg active:translate-y-0 transition ease-out duration-300'>
-							Book a table
+
+						<Link href='/book'>
+							<div className='w-30 h-30 p-3 relative border-4 border-black bg-amber-50 rounded-full hover:-translate-y-1 hover:drop-shadow-2xl active:drop-shadow-lg active:translate-y-0 transition ease-out duration-300 mr-4'>
+								<svg
+									width='100'
+									height='100'
+									viewBox='0 0 100 100'
+									xmlns='http://www.w3.org/2000/svg'>
+									<defs>
+										<path
+											id='circlePath'
+											d='
+            M 50, 50
+            m -40, 0
+            a 40,40 0 1,1 80,0
+            a 40,40 0 1,1 -80,0
+          '
+										/>
+									</defs>
+									<text>
+										<textPath
+											href='#circlePath'
+											className='font-extrabold text-xl'>
+											BOOK A TABLE
+										</textPath>
+									</text>
+								</svg>
+								<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 border-black bg-red-600 w-20 h-20 rounded-full'></div>
+							</div>
 						</Link>
 					</div>
 				</div>
