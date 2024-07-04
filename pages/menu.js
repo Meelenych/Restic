267577b4fc786fe64ac0bb10f8a92bf7421d49db
Meelenych <meelenych@gmail.com>
2024-07-04 12:33:43 -0600

    json server and axios implemented

--- pages/menu.js
+++ pages/menu.js
@@ -1,9 +1,34 @@
 import Layout from '../components/Layout';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Menu = () => {
+	const initialDishes = [];
+
+	const [dishes, setDishes] = useState(initialDishes);
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/dishes')
+			.then(res => {
+				setDishes(res.data);
+			})
+			.catch(error => {
+				console.error('Error fetching dishes:', error);
+			});
+	}, []);
+	console.log(dishes);
+
 	return (
 		<Layout>
 			<h1>Menu Page</h1>
+			<ul>
+				{dishes.map(dish => (
+					<li key={dish.id}>
+						Item {dish.id}: {dish.title}
+					</li>
+				))}
+			</ul>
 		</Layout>
 	);
 };
