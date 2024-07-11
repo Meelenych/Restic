c30f1a0043e3ccbb3469fee87a4c8ef24bec7a39
Meelenych <meelenych@gmail.com>
2024-07-10 19:46:30 -0600

    menu page updates

--- pages/menu.js
+++ pages/menu.js
@@ -23,10 +23,10 @@ const Menu = () => {
 		<Layout>
 			<div className='p-5'>
 				<h1 className='mb-5 text-xl font-semibold'>Elzar's Fine Cuisine Menu</h1>
-				<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+				<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5'>
 					{dishes.map(dish => (
 						<li key={dish.id}>
-							<div className='card glass '>
+							<div className='card glass'>
 								<figure>
 									<Image
 										src={dish.image}
@@ -36,16 +36,14 @@ const Menu = () => {
 										unoptimized
 									/>
 								</figure>
-								<div className='card-body'>
-									<h2 className='card-title line-clamp-1'>
-										Item {dish.id}: {dish.title}
-									</h2>
-									<p className='line-clamp-3'>Description: {dish.description}</p>
+								<div className='card-body p-3'>
+									<h2 className='card-title line-clamp-1'>{dish.title}</h2>
+									<p className='line-clamp-2'>{dish.description}</p>
 									<div className='w-full flex justify-between flex-col md:flex-row'>
 										<Link
 											href='/'
-											className='text-center hover:animate-pulse-glow bg-amber-500 text-white py-2 px-4 rounded-xl w-full md:w-96 md:mr-4 mb-4 md:mb-0 hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
-											Learn more
+											className='hover:text-amber-500 py-2 w-full md:w-96 md:mr-4 mb-4 md:mb-0 focus:outline-none hover:underline transition ease-in duration-300'>
+											Learn more...
 										</Link>
 										<button className='hover:animate-pulse-glow bg-indigo-500 text-white py-2 px-4 rounded-xl w-full md:w-96 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
 											Order now!
