5c74e232fca53839b6a8b0dc58435c5e915d5f8d
Meelenych <meelenych@gmail.com>
2024-07-16 00:49:22 -0600

    login page form

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const Register = () => {
+	return (
+		<Layout>
+			<div className='p-5'>
+				<h1>Register Page</h1>
+			</div>
+		</Layout>
+	);
+};
+
+export default Register;
