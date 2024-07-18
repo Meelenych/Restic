f6813821e8f12d64233fac8db938d7bc0f8c5415
Meelenych <meelenych@gmail.com>
2024-07-17 23:40:51 -0600

    register page functional

--- pages/register.js
+++ pages/register.js
@@ -1,10 +1,136 @@
 import Layout from '../components/Layout';
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Register = () => {
+	const initialState = {
+		first: '',
+		last: '',
+		email: '',
+		phone: '',
+		login: '',
+		password: '',
+	};
+
+	const [formData, setFormData] = useState(initialState);
+	const router = useRouter();
+
+	const { first, last, email, phone, login, password } = formData;
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		// Handle form submission logic here (e.g., send data to server)
+		console.log('formData', formData);
+		clearForm();
+
+		// Redirect to confirmation page with form data as query parameters
+		router.push({
+			pathname: '/',
+			query: formData,
+		});
+	};
+
+	const clearForm = () => {
+		setFormData(initialState);
+	};
 	return (
 		<Layout>
-			<div className='p-5'>
-				<h1>Register here</h1>
+			<div className='p-5 grid grid-cols-1 md:grid-cols-2 w-full'>
+				<div className='flex items-center justify-center'>
+					<Image
+						className='hidden md:block'
+						src={'webp/Lord_Nibbler.webp'}
+						alt={``}
+						width={300}
+						height={400}
+						unoptimized
+					/>
+				</div>
+				<div
+					className='p-1 md:p-8 lg:p-16 flex
+          flex-col items-center md:sitems-start justify-start'>
+					<div className='w-full md:w-96'>
+						<h1 className='text-2xl font-bold mb-4 text-indigo-600'>Register here</h1>
+						<form
+							id='booking-form'
+							onSubmit={handleSubmit}
+							className='space-y-4 text-emerald-300'>
+							<div className='grid grid-cols-1'>
+								{/* Login */}
+								<div className='flex flex-wrap border border-emerald-200 rounded-xl p-3 w-full mb-3 backdrop-blur-sm bg-black/30'>
+									<p className='w-full mb-3 font-semibold'>Please log in</p>
+									{/*Login */}
+									<div className='w-full px-2 mb-4'>
+										<label
+											htmlFor='login'
+											className='block text-sm font-medium text-purple-300'>
+											Login
+										</label>
+										<input
+											type='text'
+											id='login'
+											name='login'
+											value={formData.login}
+											onChange={handleChange}
+											required
+											className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+										/>
+									</div>
+									{/* Pssword */}
+									<div className='w-full px-2 mb-4'>
+										<label
+											htmlFor='password'
+											className='block text-sm font-medium text-purple-300'>
+											Password
+										</label>
+										<input
+											type='password'
+											id='password'
+											name='password'
+											value={formData.password}
+											onChange={handleChange}
+											required
+											className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+										/>
+									</div>
+								</div>
+							</div>
+							<div className='grid grid-cols-1'>
+								<button
+									type='button'
+									onClick={() => clearForm()}
+									className='hover:animate-pulse-glow bg-amber-500 text-white py-2 px-4 rounded-xl w-full md:w-96  mb-4 hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+									Clear form
+								</button>
+								<button
+									type='submit'
+									className='hover:animate-pulse-glow bg-indigo-500 text-white py-2 px-4 rounded-xl w-full md:w-96 mb-4 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+									Log in
+								</button>
+								<Link
+									href='/register'
+									className='hover:animate-pulse-glow bg-emerald-800 text-white text-center py-2 px-4 rounded-xl w-full md:w-96 mb-4 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+									Register
+								</Link>
+								<Link
+									href='/'
+									className='hover:animate-pulse-glow bg-red-500 text-white text-center py-2 px-4 rounded-xl w-full md:w-96 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+									Cancel
+								</Link>
+							</div>
+						</form>
+					</div>
+				</div>
 			</div>
 		</Layout>
 	);
