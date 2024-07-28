720e08d56972ca1d4b4eb8c9632889be546e2c5d
Meelenych <meelenych@gmail.com>
2024-07-27 23:51:20 -0600

    book confirmation page styles

--- pages/book_confirmation.js
+++ pages/book_confirmation.js
@@ -41,24 +41,27 @@ const BookConfirmation = () => {
 
 	return (
 		<Layout>
-			<h1>
-				Your reservation is confirmed, please refer to the information below:
-			</h1>
-			<p>
-				Name: {first} {last}
-			</p>
-			<p>Phone: {phone}</p>
-			<p>Email: {email}</p>
-			<p>Date: {date}</p>
-			<p>Time: {time}</p>
-			<p>Guests: {guests}</p>
-			<p>Special Instructions: {instructions}</p>
-
-			<Link
-				href='/'
-				className='hover:animate-pulse-glow bg-red-500 text-white text-center py-2 px-4 rounded-xl w-full md:w-96 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
-				Return to Homepage
-			</Link>
+			<div className='p-5 flex flex-col items-center'>
+				<h1 className='text-xl font-semibold'>
+					Your reservation is confirmed, please refer to the information below:
+				</h1>
+				<div>
+					<p className='mt-3'>
+						Name: {first} {last}
+					</p>
+					<p>Phone: {phone}</p>
+					<p>Email: {email}</p>
+					<p>Date: {date}</p>
+					<p>Time: {time}</p>
+					<p>Guests: {guests}</p>
+					<p>Special Instructions: {instructions}</p>
+				</div>
+				<Link
+					href='/'
+					className='mt-3 hover:animate-pulse-glow bg-red-500 text-white text-center py-2 px-4 rounded-xl w-full md:w-96 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+					Return to Homepage
+				</Link>
+			</div>
 		</Layout>
 	);
 };
