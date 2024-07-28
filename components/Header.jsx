1618ef253efa2d10fa4ee4297cc73f5106f969ee
Meelenych <meelenych@gmail.com>
2024-07-28 01:07:22 -0600

    header mobile menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,7 +25,44 @@ const Header = () => {
 		<header className={`${styles.header} ${isScrolled ? styles.scrolled : ''}`}>
 			<div className='navbar'>
 				<div className='flex-1'>
-					<Navigation layout='row' />
+					<div className='navbar-start sm:block md:hidden'>
+						<div className='dropdown'>
+							<div
+								tabIndex={0}
+								role='button'
+								className='btn btn-ghost btn-circle'>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									className='h-5 w-5'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
+										d='M4 6h16M4 12h16M4 18h7'
+									/>
+								</svg>
+							</div>
+							<ul
+								tabIndex={0}
+								className='menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow'>
+								<li>
+									<a>Homepage</a>
+								</li>
+								<li>
+									<a>Portfolio</a>
+								</li>
+								<li>
+									<a>About</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className='hidden md:block w-full'>
+						<Navigation layout='row' />
+					</div>
 				</div>
 				<div className='flex-none'>
 					<div className='dropdown dropdown-end'>
