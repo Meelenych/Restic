c9196b6dbe1cc8a7e095b2b26bae803f152d00a5
Meelenych <meelenych@gmail.com>
2024-08-04 13:25:06 -0600

    contacts page content

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,14 @@ const Contact = () => {
 		<Layout>
 			<div className='p-5'>
 				<h1>Contact Page</h1>
+				<p>Our address:</p>
+				<address>1250 Main St., Chicago, 60007 IL, USA</address>
+				<p>
+					<a href='tel:+1234567890'> Phone: (123) 456-7890</a>
+				</p>
+				<p>
+					Email: <a href='mailto:admin@example.com'>admin@example.com</a>
+				</p>
 			</div>
 		</Layout>
 	);
