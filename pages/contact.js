d675227ca3bebc2ae8de7647f76d7768c84a1a7f
Meelenych <meelenych@gmail.com>
2024-08-07 01:14:25 -0600

    contact form styles upodate and message confirmation page

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,59 @@
 import Layout from '../components/Layout';
 import GoogleMap from '../components/GoogleMap';
 import IconSelector from '../helpers/IconSelector';
+import { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Contact = () => {
+	const initialState = {
+		first: '',
+		last: '',
+		email: '',
+		message: '',
+	};
+
+	// Initialize form data with initial state
+	const [formData, setFormData] = useState(initialState);
+	const router = useRouter();
+
+	const { first, last, email, subject, message } = formData;
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		// Handle form submission logic here (e.g., send data to server)
+		console.log('formData', formData);
+		clearForm();
+
+		// Redirect to confirmation page with form data as query parameters
+		router.push({
+			pathname: '/message_confirmation',
+			query: formData,
+		});
+
+		closeModal();
+	};
+
+	const clearForm = () => {
+		setFormData(initialState);
+	};
+
+	const showModal = () => {
+		document.getElementById('booking_modal').showModal();
+	};
+
+	const closeModal = () => {
+		document.getElementById('booking_modal').close();
+	};
+
 	return (
 		<Layout>
 			<h1 className='text-2xl p-5'>Find us here</h1>
@@ -41,7 +92,156 @@ const Contact = () => {
 						</p>
 					</div>
 				</div>
-				<div className='p-5'></div>
+				<div className='p-5'>
+					<form
+						id='booking-form'
+						onSubmit={handleSubmit}
+						className='space-y-4 text-emerald-300'>
+						<div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+							{/* Personal */}
+							<div className='flex flex-wrap border border-emerald-200 rounded-xl p-3 w-full mb-3 backdrop-blur-sm bg-black/30'>
+								<p className='w-full mb-3 font-semibold'>Personal information</p>
+								{/* First name */}
+								<div className='w-full md:w-1/2 px-2 mb-4'>
+									<label
+										htmlFor='first'
+										className='block text-sm font-medium text-purple-300'>
+										First name
+									</label>
+									<input
+										type='text'
+										id='first'
+										name='first'
+										value={formData.first}
+										onChange={handleChange}
+										required
+										className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+									/>
+								</div>
+								{/* Last name */}
+								<div className='w-full md:w-1/2 px-2 mb-4'>
+									<label
+										htmlFor='last'
+										className='block text-sm font-medium text-purple-300'>
+										Last name
+									</label>
+									<input
+										type='text'
+										id='last'
+										name='last'
+										value={formData.last}
+										onChange={handleChange}
+										required
+										className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+									/>
+								</div>
+								{/* Email */}
+								<div className='w-full px-2 mb-4'>
+									<label
+										htmlFor='email'
+										className='block text-sm font-medium text-purple-300'>
+										Email
+									</label>
+									<input
+										type='email'
+										id='email'
+										name='email'
+										value={formData.email}
+										onChange={handleChange}
+										required
+										className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+									/>
+								</div>
+							</div>
+							{/* Message*/}
+							<div className='flex flex-wrap border border-emerald-200 rounded-xl p-3 w-full mb-3 backdrop-blur-sm bg-black/30'>
+								<p className='w-full mb-3 font-semibold'>Your message here: </p>
+								{/* Subject */}
+								<div className='w-full px-2 mb-4'>
+									<label
+										htmlFor='subject'
+										className='block text-sm font-medium text-purple-300'>
+										Subject
+									</label>
+									<input
+										type='text'
+										id='subject'
+										name='subject'
+										value={formData.subject}
+										onChange={handleChange}
+										required
+										className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+									/>
+								</div>
+								{/* message */}
+								<div className='w-full px-2 mb-4'>
+									<label
+										htmlFor='message'
+										className='block text-sm font-medium text-purple-300'>
+										Your message
+									</label>
+									<textarea
+										id='message'
+										name='message'
+										value={formData.message}
+										onChange={handleChange}
+										className='px-2 py-1 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50'
+									/>
+								</div>
+							</div>
+						</div>
+						<div className='w-full flex justify-between flex-col md:flex-row'>
+							<button
+								type='button'
+								onClick={() => clearForm()}
+								className='hover:animate-pulse-glow-amber bg-amber-500 text-white py-2 px-4 rounded-xl w-full md:w-96 md:mr-4 mb-4 md:mb-0 hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+								Clear form
+							</button>
+							<button
+								type='button'
+								onClick={showModal}
+								className='hover:animate-pulse-glow-indigo bg-indigo-500 text-white py-2 px-4 rounded-xl w-full md:w-96 md:mr-4 mb-4 md:mb-0 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+								Send
+							</button>
+							<Link
+								href='/'
+								className='hover:animate-pulse-glow-red bg-red-500 text-white text-center py-2 px-4 rounded-xl w-full md:w-96 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+								Cancel
+							</Link>
+						</div>
+						{/* Modal */}
+						<dialog
+							id='booking_modal'
+							className='modal'>
+							<div className='modal-box'>
+								<h3 className='font-bold text-lg'>Confirm sending your message?</h3>
+								<p className='py-4'>Please check your message.</p>
+								<div>
+									<p>
+										Name: {first} {last}
+									</p>
+									<p>Email: {email}</p>
+									<p>Subject: {subject}</p>
+									<p>Message: {message}</p>
+								</div>
+								<div className='modal-action'>
+									<button
+										type='submit'
+										form='booking-form'
+										className='hover:animate-pulse-glow-indigo bg-indigo-500 text-white py-2 px-4 rounded-xl w-full md:w-96 mr-4 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+										Confirm
+									</button>
+									<button
+										type='button'
+										onClick={() => closeModal()}
+										className='hover:animate-pulse-glow-red bg-red-500 text-white text-center py-2 px-4 rounded-xl w-full md:w-96 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+										Cancel
+									</button>
+								</div>
+							</div>
+						</dialog>
+					</form>
+				</div>
 			</div>
 		</Layout>
 	);
---
+++ pages/message_confirmation.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const MessageConfirmation = () => {
+	const router = useRouter();
+	const { first, last, email, subject, message } = router.query;
+
+	// // Import necessary libraries
+	// const sgMail = require('@sendgrid/mail');
+	// sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Set your SendGrid API key
+
+	// // Function to send booking confirmation email
+	// async function sendBookingConfirmationEmail(email, bookingDetails) {
+	// 	const msg = {
+	// 		to: email,
+	// 		from: 'your@example.com', // Use your verified sender address
+	// 		subject: 'Booking Confirmation',
+	// 		html: `<p>Hello,</p>
+	//          <p>Your booking has been confirmed with the following details:</p>
+	//          <ul>
+	//            <li>Name: ${bookingDetails.first} ${bookingDetails.last}</li>
+	//            <li>Date: ${bookingDetails.date}</li>
+	//            <li>Time: ${bookingDetails.time}</li>
+	//            <li>Guests: ${bookingDetails.guests}</li>
+	//            <li>Special Instructions: ${bookingDetails.instructions}</li>
+	//          </ul>`,
+	// 	};
+
+	// 	try {
+	// 		await sgMail.send(msg);
+	// 		console.log('Email sent successfully');
+	// 	} catch (error) {
+	// 		console.error('Error sending email:', error.toString());
+	// 	}
+	// }
+
+	// module.exports = { sendBookingConfirmationEmail };
+
+	return (
+		<Layout>
+			<div className='p-5 flex flex-col items-center'>
+				<h1 className='text-xl font-semibold'>
+					Your message is sent, please refer to the information below:
+				</h1>
+				<div>
+					<p className='mt-3'>
+						Name: {first} {last}
+					</p>
+					<p>Email: {email}</p>
+					<p>Subject: {subject}</p>
+					<p>Special Instructions: {message}</p>
+				</div>
+				<Link
+					href='/'
+					className='mt-3 hover:animate-pulse-glow bg-red-500 hover:animate-pulse-glow-red text-white text-center py-2 px-4 rounded-xl w-full md:w-96 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition ease-in duration-300'>
+					Return to Homepage
+				</Link>
+			</div>
+		</Layout>
+	);
+};
+
+export default MessageConfirmation;
