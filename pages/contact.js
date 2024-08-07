07938c4d5bb32df9596d2b6f10acb8c64b4869d8
Meelenych <meelenych@gmail.com>
2024-08-07 00:15:30 -0600

    contacts page content update

--- pages/contact.js
+++ pages/contact.js
@@ -7,10 +7,11 @@ const Contact = () => {
 			<h1 className='text-2xl p-5'>Find us here</h1>
 			<GoogleMap />
 			<div className='p-5'>
-				<p>Our address:</p>
+				<p>Location:</p>
 				<address>1250 Main St., Chicago, 60007 IL, USA</address>
+				<p>Open hours: Monday - Sunday: 12:00 PM - 11:00 PM</p>
 				<p>
-					<a href='tel:+1234567890'> Phone: (123) 456-7890</a>
+					Phone: <a href='tel:+1234567890'> (123) 456-7890</a>
 				</p>
 				<p>
 					Email: <a href='mailto:admin@example.com'>admin@example.com</a>
