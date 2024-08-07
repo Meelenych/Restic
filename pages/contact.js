34ce353e5259e96e3c9be1a902e92927e2dc355a
Meelenych <meelenych@gmail.com>
2024-08-07 00:45:03 -0600

    contact page icons

--- pages/contact.js
+++ pages/contact.js
@@ -1,21 +1,47 @@
 import Layout from '../components/Layout';
 import GoogleMap from '../components/GoogleMap';
+import IconSelector from '../helpers/IconSelector';
 
 const Contact = () => {
 	return (
 		<Layout>
 			<h1 className='text-2xl p-5'>Find us here</h1>
 			<GoogleMap />
-			<div className='p-5'>
-				<p>Location:</p>
-				<address>1250 Main St., Chicago, 60007 IL, USA</address>
-				<p>Open hours: Monday - Sunday: 12:00 PM - 11:00 PM</p>
-				<p>
-					Phone: <a href='tel:+1234567890'> (123) 456-7890</a>
-				</p>
-				<p>
-					Email: <a href='mailto:admin@example.com'>admin@example.com</a>
-				</p>
+			<div className='grid grid-cols-1 md:grid-cols-2'>
+				<div className='p-5'>
+					<div className='flex mb-4'>
+						<div className='mr-4'>
+							<IconSelector icon='pin' />
+						</div>
+						<div>
+							<p>Location:</p>
+							<address>1250 Main St., Chicago, 60007 IL, USA</address>
+						</div>
+					</div>
+					<div className='flex  mb-4'>
+						<div className='mr-4'>
+							<IconSelector icon='clock' />
+						</div>
+						<p>Open hours: Monday - Sunday: 12:00 PM - 11:00 PM</p>
+					</div>
+					<div className='flex  mb-4'>
+						<div className='mr-4'>
+							<IconSelector icon='phone' />
+						</div>
+						<p>
+							Phone: <a href='tel:+1234567890'> (123) 456-7890</a>
+						</p>
+					</div>
+					<div className='flex  mb-4'>
+						<div className='mr-4'>
+							<IconSelector icon='email' />
+						</div>
+						<p>
+							Email: <a href='mailto:admin@example.com'>admin@example.com</a>
+						</p>
+					</div>
+				</div>
+				<div className='p-5'></div>
 			</div>
 		</Layout>
 	);
