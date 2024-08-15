01ce51e7838e282f61460c340238004ca4c9fbed
Meelenych <meelenych@gmail.com>
2024-08-15 01:34:15 -0600

    code cleaning

--- pages/login.js
+++ pages/login.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useSelector, useDispatch } from 'react-redux';
-import { logIn, logOut } from '../redux/auth/authSlice';
+import { logIn } from '../redux/auth/authSlice';
 
 const Login = () => {
 	const initialState = {
