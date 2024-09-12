e60b0633b7aa5df6dd12fca5bfd4bcc123ee8d88
Meelenych <meelenych@gmail.com>
2024-09-11 23:34:45 -0600

    header cart real info added

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,6 +11,8 @@ const Header = () => {
 	const dispatch = useDispatch();
 	const loggedIn = useSelector(state => state.auth.loggedIn);
 
+	const cartItems = useSelector(state => state.cart.items);
+
 	const handleLogout = () => {
 		dispatch(logOut());
 	};
@@ -102,15 +104,22 @@ const Header = () => {
 											d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
 										/>
 									</svg>
-									<span className='badge badge-sm indicator-item'>8</span>
+									<span className='badge badge-sm indicator-item'>
+										{cartItems.length}
+									</span>
 								</div>
 							</div>
 							<div
 								tabIndex={0}
 								className='card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow'>
 								<div className='card-body'>
-									<span className='text-lg font-bold'>8 Items</span>
-									<span className='text-info'>Subtotal: $999</span>
+									<span className='text-lg font-bold'>{cartItems.length} Items</span>
+									<span className='text-info'>
+										Subtotal: $
+										{cartItems
+											.reduce((acc, item) => acc + item.price * item.quantity, 0)
+											.toFixed(2)}
+									</span>
 									{/* To do: idpage left side - cart right side */}
 									<div className='card-actions'>
 										<Link
